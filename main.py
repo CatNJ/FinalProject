73a489b41c3ac9e70ec33e6326e216d330074f07
CatNJ <94224244+CatNJ@users.noreply.github.com>
2024-02-24 22:06:35 +0300

    add player

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from pygame import *
 from random import randint
+from classes.Player import Player
 
 WIN_WIDTH, WIN_HEIGHT = 900, 600
 FPS = 60
@@ -13,22 +14,27 @@ class Game():
         self.window = display.set_mode((WIN_WIDTH, WIN_HEIGHT))
         display.set_caption("Zombie Killer")
 
+        self.clock = time.Clock()
+        self.run = True
+
+        self.player = Player(self.window, WIN_WIDTH/2, WIN_HEIGHT/2, 10, 10, (0, 100, 0))
+
     def loop(self):
-        pass
+        self.player.update()
+        self.player.draw()
 
     def start(self):
-        clock = time.Clock()
-        run = True
+        while self.run:
+            self.window.fill((0, 0, 0))
 
-        while run:
             for e in event.get():
                 if e.type == QUIT:
-                    run = False
+                    self.run = False
 
             self.loop()
 
             display.update()
-            clock.tick(FPS)
+            self.clock.tick(FPS)
 
 if __name__ == "__main__":
     game = Game()

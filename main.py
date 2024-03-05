00896ca4591a6225cfef6ec6e0f8289e90d12cbb
CatNJ <94224244+CatNJ@users.noreply.github.com>
2024-03-05 21:07:14 +0300

    add custom cursor

--- main.py
+++ main.py
@@ -13,6 +13,8 @@ class Game():
         font.init()
         mixer.init()
 
+        mouse.set_visible(False)
+
         self.window = display.set_mode((WIN_WIDTH, WIN_HEIGHT))
         display.set_caption("Zombie Killer")
 
@@ -20,15 +22,24 @@ class Game():
         self.run = True
 
         self.bg = scale(load("sprites/grass.png").convert(), (WIN_WIDTH, WIN_HEIGHT))
+        self.cursor_image = scale(load("sprites/cursor.png"), (64, 64))
         self.player = Player(self.window, "sprites/player/player_pistol.png",
                              WIN_WIDTH/2, WIN_HEIGHT/2,
-                             28*2, 21*2)
+                             28*2.5, 21*2.5)
+        
+        
+    def draw_cursor(self):
+        mouse_x, mouse_y = mouse.get_pos()
+        image_width, image_height = self.cursor_image.get_size()
+        self.window.blit(self.cursor_image, (mouse_x-(image_width/2), mouse_y-(image_height/2)))
 
     def loop(self):
         self.player.move()
         self.player.draw()
         self.player.rotate(mouse.get_pos())
         self.player.shoot()
+
+        self.draw_cursor()
 
     def start(self):
         while self.run:

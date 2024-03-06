2a25e18e2dcf6619dacc334746d71437d5795acf
CatNJ <94224244+CatNJ@users.noreply.github.com>
2024-03-06 19:42:43 +0300

    add Enemy class

---
+++ classes/Enemy.py
@@ -0,0 +1,22 @@
+import math
+import pygame
+from classes.GameSprite import GameSprite
+
+class Enemy(GameSprite):
+    def __init__(self, window, image, x, y, width, height, target):
+        super().__init__(window, image, x, y, width, height)
+        self.target = target
+        self.speed = 2
+
+    def move(self):
+        try:
+            dx, dy = (self.target.rect.x - self.rect.x,
+                      self.target.rect.y - self.rect.y)
+            dist = math.hypot(dx, dy)
+            dx, dy = dx / dist, dy / dist
+            self.rect.x += dx * self.speed
+            self.rect.y += dy * self.speed
+            self.rotate(self.target.get_pos())
+        except ZeroDivisionError:
+            pass
+
